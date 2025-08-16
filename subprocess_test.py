f9448f3961cc1bd105b56c070bdb06d064b41303
maykonjonattan <maykinvest@outlook.com>
2025-08-16 13:15:12 -0300

    Add some funtionalities to the code, it will use the scan 'sfc /scannow' to check if the computer has any problem!

--- subprocess_test.py
+++ subprocess_test.py
@@ -6,3 +6,17 @@ print(f'Trying to delete folder: {folder}')
 
 delete = subprocess.run(f"rmdir /s /q '{folder}'", shell=True)
 
+if delete.returncode == 0:
+    print("Foldedr deleted successfully.")
+
+else:
+    print(f"Failed to delete foldedr. return code: {delete.returncode}")
+    
+print("\nRunning: sfc /scannow\n")
+scan = subprocess.run("sfc /scannow", shell=True)
+
+if scan.returncode == 0:
+    print("\n Scan completedd successfully.")
+
+else:
+    print(f"Scan failed or foundd issues. Return code: {scan.returncode}")
